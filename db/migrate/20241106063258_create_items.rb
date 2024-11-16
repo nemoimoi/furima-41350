71239d1364f94ec20efa853c312f8b0c53ca6f9c
nemoimoi <nemoto.mikako@gmail.com>
2024-11-16 14:27:25 +0900

    商品出品機能デバック4

--- db/migrate/20241106063258_create_items.rb
+++ db/migrate/20241106063258_create_items.rb
@@ -8,7 +8,7 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.integer    :area_id,     null:false
       t.integer    :deadline_id, null:false
       t.integer    :price,       null:false
-      t.references :user,        null:false,foregign_key: true
+      t.references :user,        null:false,foreign_key: true
       t.timestamps
     end
   end
