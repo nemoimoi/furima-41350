d84f51dd4ce5921e8480ed5bc0839bd1713575b6
nemoimoi <nemoto.mikako@gmail.com>
2024-11-25 04:10:34 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create]
+  before_action :authenticate_user!, only:[:edit, :new, :create]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -23,9 +23,35 @@ class ItemsController < ApplicationController
     @user = @item.user
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    set_item_collections
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+       redirect_to root_path
+    else
+      set_item_collections
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :title, :description, :category_id, :condition_id, :postage_id, :area_id, :deadline_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item_collections
+    @categories = Category.all
+    @conditions = Condition.all
+    @postages = Postage.all
+    @areas = Area.all
+    @deadlines = Deadline.all
   end
 end
